54b039d8ba491fa8ad243fcfce82be7276f60115
uhjrdijrfsisfhjnk <43994096+uhjrdijrfsisfhjnk@users.noreply.github.com>
2018-10-09 23:51:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -320,13 +320,6 @@ client.on('message', message => {
     }
 });
 
-client.on('message',function(message) {
-	let prefix = "!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
+
 
 client.login(process.env.BOT_TOKEN);
